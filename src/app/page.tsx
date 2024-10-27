e9df73725fb00645ca30e2b1f861ffc1a3a6017a
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2024-10-27 17:17:56 +0100

    create initial banner version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import '@/app/globals.css';
+import Banner from "@/components/Banner";
 
 export default function Home() {
 
   return (
     <div className="w-full">
       <Header />
+      <Banner />
     </div>
   );
 }
