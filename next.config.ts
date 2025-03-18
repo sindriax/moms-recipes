f068b5ac08e2dd73058357a6f00ea049b27c2dc9
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2025-03-18 21:26:39 +0100

    update images and adjust header height; add new logo and chef image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["www.bylena.com"],
+    domains: ["www.bylena.com", "i.ibb.co"],
   },
 };
 
