c7be5cd44a5cdfd5fc7df2b8c6699bb12ed23ae1
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2025-03-22 21:20:15 +0100

    add advertisement component to shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Items from "../../data/shop";
 import ShopItemDetail from "@/components/shop/ItemDetail";
 import ShopItemCard from "@/components/shop/ItemCard";
+import Adevertisement from "@/components/shop/Advertisement";
 
 const ShopPage: React.FC = () => {
   const [selectedItem, setSelectedItem] = useState<number | null>(null);
@@ -17,22 +18,28 @@ const ShopPage: React.FC = () => {
       : null;
 
   return (
-    <div className="p-8">
-      {selectedItemData ? (
-        <ShopItemDetail item={selectedItemData} />
-      ) : (
-        <div>
-          <h1 className="text-3xl font-bold text-gray-800 mb-6">Shop</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {Items.map((item) => (
-              <ShopItemCard
-                key={item.id}
-                item={{ ...item, onClick: handleCardClick }}
-              />
-            ))}
+    <div>
+      <div className="p-8">
+        {selectedItemData ? (
+          <ShopItemDetail item={selectedItemData} />
+        ) : (
+          <div>
+            <h1 className="text-3xl font-bold text-gray-800 mb-6">
+              Tiendecita Oficial
+            </h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              {Items.map((item) => (
+                <ShopItemCard
+                  key={item.id}
+                  item={{ ...item, onClick: handleCardClick }}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+
+      <Adevertisement />
     </div>
   );
 };
