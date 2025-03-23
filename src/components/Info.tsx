1de5259f7b6b2b25c837b686906967d2f5546075
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2025-03-23 19:05:59 +0100

    replace chef image

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import ChefImage from "/public/chefita.png";
+import ChefImage from "/public/arancita.png";
 
 const Info = () => {
   return (
