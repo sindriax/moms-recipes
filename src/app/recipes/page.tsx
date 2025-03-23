4a804b62a8d95e70f295147ec865f80fca6c2782
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2025-03-23 18:33:08 +0100

    refactor recipes for improved responsiveness and styling

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -17,13 +17,16 @@ const RecipesPage: React.FC = () => {
       : null;
 
   return (
-    <div className="p-8">
+    <div className="max-w-screen-xl mx-auto px-4 sm:px-8 py-8">
       {selectedRecipeData ? (
         <RecipesDetails recipe={selectedRecipeData} />
       ) : (
-        <div>
-          <h1 className="text-3xl font-bold text-gray-800 mb-6">Recetas</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="relative inline-block text-2xl sm:text-4xl font-bold text-gray-800 break-words">
+            Recetas{" "}
+            <span className="absolute left-0 -bottom-1 h-1 bg-orange-200 inline-block underline-animate"></span>
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
             {Recipes.map((recipe) => (
               <RecipesCard
                 key={recipe.id}
