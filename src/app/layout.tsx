942de6a9316d541e1963dc2c3eeca5cf41890b28
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2025-03-23 09:41:23 +0100

    add lucide-react dependency and improve layout and banner styling for better responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex flex-col items-center justify-center">
+      <body className="min-h-screen flex flex-col w-full">
         <Header />
         {children}
         <Footer />
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,17 +6,17 @@ import { motion } from "framer-motion";
 export default function Banner() {
   return (
     <motion.section
-      className="flex flex-col-reverse md:flex-row items-center bg-gray-100 p-8 shadow-lg h-400 mt-20 mr-20 ml-20 border border-orange-300 rounded-lg transition-transform duration-300"
+      className="flex flex-col-reverse md:flex-row items-center bg-gray-100 p-4 sm:p-8 shadow-lg mt-10 mx-4 sm:mx-20 border border-orange-300 rounded-lg transition-transform duration-300"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       whileHover={{ scale: 1.02, backgroundColor: "#FFF7ED" }}
       transition={{ duration: 1, ease: "easeOut" }}
     >
       <div className="md:w-1/2 text-center md:text-left">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4 ml-20">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4 ml-0 md:ml-20 text-center md:text-left">
           Conoce a la Chef Arantxa
         </h2>
-        <p className="ml-20 text-lg text-gray-600">
+        <p className="ml-0 md:ml-20 text-lg text-gray-600 text-center md:text-left">
           Arantxa es una famosa chef de Mataporquera, ahora reconocida como chef
           con estrella Michelin en Cataluña. Su talento en la cocina abarca
           desde deliciosos canelones hasta asados perfectos. Sabe preparar de
@@ -25,7 +25,13 @@ export default function Banner() {
         </p>
       </div>
       <div className="md:w-1/2 flex justify-center md:justify-end mt-6 md:mt-0">
-        <Image src={Chef} alt="Chef Arantxa" height={600} />
+        <Image
+          src={Chef}
+          alt="Chef Arantxa"
+          width={300}
+          height={300}
+          className="w-auto h-auto max-h-[300px] sm:max-h-[400px]"
+        />
       </div>
     </motion.section>
   );
